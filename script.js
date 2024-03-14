447527b42a1e43e5491d70a6a6454c8701e5799b
anish797 <anishlol797@gmail.com>
2024-03-14 13:14:56 +0530

    wip

--- script.js
+++ script.js
@@ -1,67 +1,81 @@
+const result = document.querySelector(".result");
+const score = document.querySelector(".score")
+const finalResult = document.querySelector(".finalResult");
+const player = document.querySelector(".player");
+const comp = document.querySelector(".comp");
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice(){
   let computerChoice=Math.ceil(Math.random()*3);
   if(computerChoice==1){
+    comp.innerHTML="";
+    comp.innerHTML=`<img src="./images/rock-removebg-preview.png" alt="rock"></img>`
     return "Rock";
   } else if(computerChoice==2){
+    comp.innerHTML="";
+    comp.innerHTML=`<img src="./images/rock-removebg-preview.png" alt="rock"></img>`
     return "Paper";
   } else if(computerChoice==3){
+    comp.innerHTML="";
+    comp.innerHTML=`<img src="./images/rock-removebg-preview.png" alt="rock"></img>`
     return "Scissors";
   }
 }
 
-function playRound(playerChoice,computerChoice,userScore){
+function playRound(playerChoice){
+  let computerChoice=getComputerChoice().toLowerCase();;
   if(playerChoice=="rock" && computerChoice=="paper"){
-    console.log("You Lose! Paper beats Rock");
-    return -1;
+    player.innerhtml=`<img src="./images/rock-removebg-preview.png" alt="rock"></img>`;
+    result.textContent= "You Lose! Paper beats Rock";
+    computerScore++;
   } else if(playerChoice=="rock" && computerChoice=="scissors"){
-      console.log("You Win! Rock beats Scissors");
-      return 1;
-  } else if(playerChoice=="rock" && computerChoice=="rock"){
-      console.log("It's a draw, no one wins!");
-      return 0;
-  } else if(playerChoice=="paper" && computerChoice=="rock"){
-      console.log("You Win! Paper beats Rock");
-      return 1;
-  } else if(playerChoice=="paper" && computerChoice=="paper"){
-      console.log("It's a draw, no one wins!");
-      return 0;
-  } else if(playerChoice=="paper" && computerChoice=="scissors"){
-      console.log("You Lose! Scissors beats Paper");
-      return -1;
-  } else if(playerChoice=="scissors" && computerChoice=="rock"){
-      console.log("You Lose! Rock beats Scissors");
-      return -1;
-  } else if(playerChoice=="scissors" && computerChoice=="paper"){
-      console.log("You Win! Scissors beats Paper");
-      return 1;
-  } else if(playerChoice=="scissors" && computerChoice=="scissors"){
-      console.log("It's a draw, no one wins!");
-      return 0;
-  } else{
-    console.log("Invalid choice");
-  }
-}
-
-function playGame(){
-  let playerScore = 0;
-  let computerScore = 0;
-  for(let i=1;i<=5;i++){
-    let playerChoice=prompt("Enter your choice").toLowerCase();
-    let computerChoice=getComputerChoice().toLowerCase();
-    let winner = playRound(playerChoice,computerChoice);
-    if(winner===1){
+      player.innerhtml=`<img src="./images/rock-removebg-preview.png" alt="rock"></img>`;
+      result.textContent= "You Win! Rock beats Scissors";
       playerScore++;
-    } else if(winner===-1){
+  } else if(playerChoice=="rock" && computerChoice=="rock"){
+      player.innerhtml=`<img src="./images/rock-removebg-preview.png" alt="rock"></img>`;
+      result.textContent= "It's a draw, no one wins!";
+  } else if(playerChoice=="paper" && computerChoice=="rock"){
+      result.textContent= "You Win! Paper beats Rock";
+      playerScore++;
+  } else if(playerChoice=="paper" && computerChoice=="paper"){
+      result.textContent= "It's a draw, no one wins!";
+  } else if(playerChoice=="paper" && computerChoice=="scissors"){
+      result.textContent= "You Lose! Scissors beats Paper";
       computerScore++;
-    }
+  } else if(playerChoice=="scissors" && computerChoice=="rock"){
+      result.textContent= "You Lose! Rock beats Scissors";
+      computerScore++;
+  } else if(playerChoice=="scissors" && computerChoice=="paper"){
+      result.textContent= "You Win! Scissors beats Paper";
+      playerScore++;
+  } else if(playerChoice=="scissors" && computerChoice=="scissors"){
+      result.textContent= "It's a draw, no one wins!";
   }
-  if(playerScore>computerScore){
-    console.log(`The score is ${playerScore}-${computerScore}. You Win!`);
-  } else if(playerScore<computerScore){
-    console.log(`The score is ${playerScore}-${computerScore}. You Lose!`);
-  } else{
-    console.log(`The score is ${playerScore}-${computerScore}. It's a Draw!`);
+  score.textContent=`${playerScore}-${computerScore}`;
+  if(playerScore===5){
+    if(playerScore>computerScore){
+      finalResult.textContent="You Win!";
+    }else if(playerScore<computerScore){
+      finalResult.textContent="You Lose!";
+    }else{
+      finalResult.textContent="It's a Draw!";
+    }
+    playerScore=0;
+    computerScore=0;
+    score.textContent=`${playerScore}-${computerScore}`;
   }
 }
 
-playGame();
+const buttons = document.querySelectorAll(".playerChoices");
+buttons.forEach((button)=>{
+  button.addEventListener("click",()=>{
+    let choice=button.getAttribute("data-choice");
+    playRound(choice);
+  });
+});
+
+if(playerScore===5){
+  console.log("hi")
+}
